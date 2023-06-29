e5d82f22a93771a977fd18f6f60e040f2d8c867d
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-29 12:37:13 +0700

    Constructor for the node class Node(string i, Node* 1, Node* r)

--- binary search diary/binary search diary.cpp
+++ binary search diary/binary search diary.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <string> using namespace std;
+#include <string> 
+using namespace std;
+
 class Node
 {
 public:
 	string info;
-	Node* leftchild; Node* rightchild;
+	Node* leftchild;
+	Node* rightchild;
 	// Constructor for the node class Node(string i, Node* 1, Node* r)
 	{
 		info = i;
-		leftchild = 1; right child = r;
+		leftchild = 1;
+		right child = r;
 	}
 };
 class BinaryTree
