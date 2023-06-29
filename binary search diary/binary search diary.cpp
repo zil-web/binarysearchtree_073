af661cbfeadcc6593ffb0d8b9a8c63ad1800f788
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-29 12:34:47 +0700

    Add project files.

---
+++ binary search diary/binary search diary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string> using namespace std;
+class Node
+{
+public:
+	string info;
+	Node* leftchild; Node* rightchild;
+	// Constructor for the node class Node(string i, Node* 1, Node* r)
+	{
+		info = i;
+		leftchild = 1; right child = r;
+	}
+};
+class BinaryTree
+{
+public:
+	Node* ROOT;
+	Binary Tree()
+	{
+		ROOT = NULL; // Initializing ROOT to null
+}
