946d7defec6716b13d7e117ae18f49ee3127c67f
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-29 12:40:01 +0700

    node root

--- binary search diary/binary search diary.cpp
+++ binary search diary/binary search diary.cpp
@@ -8,18 +8,22 @@ public:
 	string info;
 	Node* leftchild;
 	Node* rightchild;
-	// Constructor for the node class Node(string i, Node* 1, Node* r)
+
+	// Constructor for the node class Node
+	(string i, Node* 1, Node* r)
 	{
 		info = i;
 		leftchild = 1;
 		right child = r;
 	}
 };
+
 class BinaryTree
 {
 public:
 	Node* ROOT;
-	Binary Tree()
+
+	BinaryTree()
 	{
 		ROOT = NULL; // Initializing ROOT to null
-}
+	}
