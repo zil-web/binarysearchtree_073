c58d554d36ee3ea09dcab3d3a162f427aebe3cca
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-29 13:03:28 +0700

    if the value in the data field of the new node is greater than that of the parent

--- binary search diary/binary search diary.cpp
+++ binary search diary/binary search diary.cpp
@@ -49,3 +49,6 @@ public:
 		{
 			parent->leftchild = newNode; //make the left child of the parent point to the new node
 		}
+		else if (element > parent->info) // if the value in the data field of the new node is greater than that of the parent 
+		{
+			parent->rightchild = newNode; //make
