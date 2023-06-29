2795f904aba73b0367ac432bbbfdf8a9341132b3
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-29 12:49:30 +0700

    make to the data field of the new node

--- binary search diary/binary search diary.cpp
+++ binary search diary/binary search diary.cpp
@@ -27,3 +27,11 @@ public:
 	{
 		ROOT = NULL; // Initializing ROOT to null
 	}
+
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node (element, NULL, NULL); // Allocate memory for the new node newNode->info = element; // Assign value to the data field of the new node newNode->left child = NULL; // Make the left child of the new node point to NULL newNode->rightchild = NULL; // Make the right child of the new node point to NULL
+		newNode->info = element; // assign value to the data field of the new node
+		newNode->leftchild = NULL; //make to the data field of the new node
+	    search (element, parent, current Node); // Locate the node which will be the parent of the node to be inserted
+	   if (parent == NULL) // If the parent is NULL (Tree is empty)
